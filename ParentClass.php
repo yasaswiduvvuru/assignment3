4cb55f68aeb6d5b813b2e1dd2e9838a79f027f45
yasaswiduvvuru <yrd3@pitt.edu>
2017-04-03 09:26:34 -0400

    Update ParentClass.php

--- ParentClass.php
+++ ParentClass.php
@@ -1,6 +1,40 @@
 <?php
 	// This is the file for the parent class
-
-	class ParentClass {
-		
+class Student
+{
+	protected $first;
+	protected $last;
+	protected $username;
+	protected $peoplesoft;
+	
+	public function __construct($first, $last, $username, $peoplesoft)
+	{
+		$this->$first = $first;
+		$this->$last = $last;
+		$this->$username = $username;
+		$this->$peoplesoft = $peoplesoft;
 	}
+	
+	public function getFirstName()
+	{
+		return $this->$first;	
+	}
+	
+	public function getLastName()
+	{
+		return $this->$last;
+	}
+	
+	public function getUsername()
+	{
+		return $this->$username;	
+	}
+	
+	public function getPeoplesoft()
+	{
+		return $this->$peoplesoft;	
+	}
+
+}
+
+?>
